e4c84ff25f3d0d5325cd685cb7e05b3ce6641e1d
leolanese <laneseleo@gmail.com>
2024-10-17 16:49:21 +0100

    Working after cherry-pick + added destroyRef

--- src/app/components/autocompleteTrieWebWorkerApi/autocomplete-trie-web-worker-api.component.ts
+++ src/app/components/autocompleteTrieWebWorkerApi/autocomplete-trie-web-worker-api.component.ts
@@ -1,9 +1,10 @@
 import {CommonModule} from '@angular/common';
-import {Component,OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Subject,debounceTime} from 'rxjs';
 import {SuggestionListComponent} from "./suggestionList.component";
 import {TrieService} from './trie.service';
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-autocomplete-trie-web-worker-api',
@@ -17,39 +18,47 @@ import {TrieService} from './trie.service';
     <main>
       <h1>Autocomplete Angular: Using Trie + WW + API</h1>
 
-      <input type="text" 
-        placeholder="Enter city (Andorra, etc)" 
-        [(ngModel)]="input" 
+      <input type="text"
+        placeholder="Enter city (Andorra, etc)"
+        [(ngModel)]="input"
         (input)="onInputChange($event)"
-        (keyup.enter)="handleAddWord()" 
+        (keyup.enter)="handleAddWord()"
       />
 
       <button (click)="handleAddWord()">Add Word</button>
 
       <!-- Suggestions List -->
       <suggestion-list [suggestions]="suggestions" />
-    
+
     </main>
     `,
 })
 export class AutocompleteTrieWebWorkerApiComponent implements OnInit {
   suggestions: string[] = [];
   input: string = '';
-  
+
   private inputSubject = new Subject<string>();
+  private destroyRef = inject(DestroyRef)
 
   constructor(private trieService: TrieService) {}
 
   ngOnInit(): void {
-    // Initialize the trie with world cities
-    this.trieService.initializeTrie().subscribe({
+    // Initialise the trie with world cities
+    this.trieService.initializeTrie().pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe({
       next: (msg) => console.log(msg),
-      error: (err) => console.error('Initialization error:', err),
+      error: (err) => console.error('Initialisation error:', err),
     });
 
     // Set up debounce for input
-    this.inputSubject.pipe(debounceTime(300)).subscribe((prefix) => {
-      this.trieService.getSuggestions(prefix).subscribe({
+    this.inputSubject.pipe(
+      debounceTime(300),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe((prefix) => {
+      this.trieService.getSuggestions(prefix).pipe(
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe({
         next: (suggestions) => (this.suggestions = suggestions),
         error: (err) => console.error('Error fetching suggestions:', err),
       });
