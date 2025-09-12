082bcf950538d8a22fc81da16f3e590f1d549eb7
leolanese <laneseleo@gmail.com>
2025-09-12 17:09:55 +0100

    angular updated

--- src/app/components/autocompleteTrieWebWorkerApi/autocomplete-trie-web-worker-api.component.ts
+++ src/app/components/autocompleteTrieWebWorkerApi/autocomplete-trie-web-worker-api.component.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {Component,DestroyRef,inject,OnInit} from '@angular/core';
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {FormsModule} from '@angular/forms';
-import {debounceTime,Subject} from 'rxjs';
+import {debounceTime,Subject,switchMap} from 'rxjs';
 import {SuggestionListComponent} from "./suggestionList.component";
 import {TrieService} from './trie.service';
 
@@ -54,15 +54,13 @@ export class AutocompleteTrieWebWorkerApiComponent implements OnInit {
     // Set up debounce for input
     this.inputSubject.pipe(
       debounceTime(300),
+      switchMap(prefix => this.trieService.getSuggestions(prefix)),
       takeUntilDestroyed(this.destroyRef)
-    ).subscribe((prefix) => {
-      this.trieService.getSuggestions(prefix).pipe(
-        takeUntilDestroyed(this.destroyRef)
-      ).subscribe({
-        next: (suggestions) => (this.suggestions = suggestions),
-        error: (err) => console.error('Error fetching suggestions:', err),
-      });
+    ).subscribe({
+      next: suggestions => this.suggestions = suggestions,
+      error: err => console.error('Error fetching suggestions:', err),
     });
+
   }
 
   onInputChange(event: Event): void {
